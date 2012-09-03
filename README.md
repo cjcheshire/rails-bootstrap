c35e0dbf1d6c552b50ddd63f69a32ede6d601963
Chris Cheshire <chris.cheshire@gmail.com>
2012-09-03 17:22:52 +0100

    added ie responsive fix

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ rails-bootstrap
 ===============
 
 My default setup just the way I like it
+
+Implements Jake Archibald's IE [SASS IE](http://jakearchibald.github.com/sass-ie/ "Visit Jake's ReadMe")
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,7 @@ RailsBootstrap::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( all-old-ie.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
