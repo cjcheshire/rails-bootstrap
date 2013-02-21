7753aa8e2f104cb394446e583a943b1c085f0d77
Chris Cheshire <chris.cheshire@gmail.com>
2013-02-21 17:28:26 +0000

    font stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,10 @@ RailsBootstrap::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+
+  config.assets.precompile += %w( .svg .eot .woff .ttf )	
+
   config.assets.precompile += %w( all-old-ie.css )
 
   # Disable delivery errors, bad email addresses will be ignored
